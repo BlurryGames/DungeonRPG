5a02dcbb4c2a58c40097654032e6830177d00dd0
BlurryGames <julian.lb.games@gmail.com>
2024-06-04 18:36:53 +0200

    Section2: Player Movement / Lesson10: Flipping the Player

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -20,6 +20,8 @@ public partial class Player : CharacterBody3D
         Velocity *= 5.0f;
 
         MoveAndSlide();
+
+        Flip();
     }
 
     public override void _Input(InputEvent @event)
@@ -34,5 +36,17 @@ public partial class Player : CharacterBody3D
         {
             animationPlayer.Play(GameConstants.ANIM_MOVE);
         }
+    }
+
+    private void Flip()
+    {
+        bool isNotMovingHorizontally = Velocity.X == 0.0f;
+        if (isNotMovingHorizontally)
+        {
+            return;
+        }
+
+        bool isMovingLeft = Velocity.X < 0.0f;
+        sprite3D.FlipH = isMovingLeft;
     }
 }
