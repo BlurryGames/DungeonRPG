d37aaf7134e3a2a0fdcd99c812c141583fced7f0
BlurryGames <julian.lb.games@gmail.com>
2024-06-04 18:08:07 +0200

    Section2: Player Movement / Lesson7: Exporting Fields with Attributes

--- Scripts/Player/Player.cs
+++ Scripts/Player/Player.cs
@@ -3,7 +3,17 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
+    [ExportGroup("Required Nodes")]
+    [Export] private AnimationPlayer animationPlayer = null;
+    [Export] private Sprite3D sprite3D = null;
+
     private Vector2 direction = Vector2.Zero;
+
+    public override void _Ready()
+    {
+        GD.Print(animationPlayer.Name);
+        GD.Print(sprite3D.Name);
+    }
 
     public override void _PhysicsProcess(double delta)
     {
