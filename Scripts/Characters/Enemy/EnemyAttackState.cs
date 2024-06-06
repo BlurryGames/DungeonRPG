b277c6abb970129aa2998706b14b36207d2fb48e
BlurryGames <julian.lb.games@gmail.com>
2024-06-06 21:59:36 +0200

    Section6: Combat System / Lesson13: Moving the Enemy Hitbox

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -1,9 +1,22 @@
 using Godot;
+using System.Linq;
 
 public partial class EnemyAttackState : EnemyState
 {
+    private Vector3 targetPosition = Vector3.Zero;
+
     protected override void EnterState()
     {
         character.AnimationPlayer.Play(GameConstants.ANIM_ATTACK);
+
+        Node3D target = character.attackArea.GetOverlappingBodies().First();
+
+        targetPosition = target.GlobalPosition;
+    }
+
+    private void PerformHit()
+    {
+        character.ToggleHitbox(false);
+        character.Hitbox.GlobalPosition = targetPosition;
     }
 }
