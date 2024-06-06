64e9b72144d431903e140e6e408b5ed7d606e703
BlurryGames <julian.lb.games@gmail.com>
2024-06-06 22:14:09 +0200

    Section6: Combat System / Lesson14: Finishing the Enemy Attack State

--- Scripts/Characters/Enemy/EnemyAttackState.cs
+++ Scripts/Characters/Enemy/EnemyAttackState.cs
@@ -12,11 +12,43 @@ public partial class EnemyAttackState : EnemyState
         Node3D target = character.attackArea.GetOverlappingBodies().First();
 
         targetPosition = target.GlobalPosition;
+
+        character.AnimationPlayer.AnimationFinished += HandleAnimationFinished;
+    }
+
+    protected override void ExitState()
+    {
+        character.AnimationPlayer.AnimationFinished -= HandleAnimationFinished;
     }
 
     private void PerformHit()
     {
         character.ToggleHitbox(false);
         character.Hitbox.GlobalPosition = targetPosition;
+    }
+
+    private void HandleAnimationFinished(StringName animationName)
+    {
+        character.ToggleHitbox(true);
+
+        Node3D target = character.attackArea.GetOverlappingBodies().FirstOrDefault();
+        if (target == null)
+        {
+            Node3D chaseTarget = character.chaseArea.GetOverlappingBodies().FirstOrDefault();
+            if (chaseTarget == null)
+            {
+                character.StateMachine.SwitchState<EnemyReturnState>();
+                return;
+            }
+
+            character.StateMachine.SwitchState<EnemyChaseState>();
+            return;
+        }
+
+        character.AnimationPlayer.Play(GameConstants.ANIM_ATTACK);
+        targetPosition = target.GlobalPosition;
+
+        Vector3 direction = character.GlobalPosition.DirectionTo(targetPosition);
+        character.Sprite3D.FlipH = direction.X < 0.0f;
     }
 }
