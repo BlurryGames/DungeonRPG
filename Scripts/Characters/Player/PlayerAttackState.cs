3811ff4efeda6618aca8e42bdb427a2e057cd1af
BlurryGames <julian.lb.games@gmail.com>
2024-06-06 09:03:55 +0200

    Section6: Combo System / Lesson4: Resetting the Combo Counter With a Timer

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -2,12 +2,21 @@ using Godot;
 
 public partial class PlayerAttackState : PlayerState
 {
+    [Export] private Timer comboTimer = null;
+
     private int comboCounter = 1;
     private int maxComboCount = 2;
 
+    public override void _Ready()
+    {
+        base._Ready();
+
+        comboTimer.Timeout += () => comboCounter = 1;
+    }
+
     protected override void EnterState()
     {
-        character.AnimationPlayer.Play(GameConstants.ANIM_ATTACK + comboCounter.ToString());
+        character.AnimationPlayer.Play(GameConstants.ANIM_ATTACK + comboCounter.ToString(), -1.0, 1.5f);
 
         character.AnimationPlayer.AnimationFinished += HandleAnimationFinished;
     }
@@ -15,6 +24,8 @@ public partial class PlayerAttackState : PlayerState
     protected override void ExitState()
     {
         character.AnimationPlayer.AnimationFinished -= HandleAnimationFinished;
+
+        comboTimer.Start();
     }
 
     private void HandleAnimationFinished(StringName animationName)
