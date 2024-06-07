4a635a73a4f8c6846928268aa98dc9f4cd0b9205
BlurryGames <julian.lb.games@gmail.com>
2024-06-07 18:19:55 +0200

    Section8: Finishing Touches / Lesson3: Creating a Reward Resource

---
+++ Scripts/Reward/RewardResource.cs
@@ -0,0 +1,13 @@
+using Godot;
+
+[GlobalClass]
+public partial class RewardResource : Resource
+{
+    [Export] public Texture2D spriteTexture { get; private set; } = null;
+
+    [Export] public string Description { get; private set; } = null;
+
+    [Export(PropertyHint.Range, "1.0f, 100.0f, 1.0f")] public float Amount { get; private set; } = 0.0f;
+
+    [Export] public Stat targetStat { get; private set; } = default;
+}
---
+++ Scripts/Reward/TreasureChest.cs
@@ -0,0 +1,24 @@
+using Godot;
+
+public partial class TreasureChest : StaticBody3D
+{
+    [Export] private Area3D area = null;
+    [Export] private Sprite3D sprite = null;
+    [Export] private RewardResource reward = null;
+
+    public override void _Ready()
+    {
+        area.BodyEntered += b => sprite.Visible = true;
+        area.BodyExited += b => sprite.Visible = false;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (!area.HasOverlappingBodies() || !Input.IsActionJustPressed(GameConstants.INPUT_INTERACT))
+        {
+            return;
+        }
+
+        GD.Print("Opened!");
+    }
+}
